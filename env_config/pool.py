cbce19c10215c749fcdaf560cc3647a73793740d
ChrisPaulBennett <christopher.bennett@metoffice.gov.uk>
2021-01-28 16:40:08 +0000

    Switched to committing to master

--- env_config/pool.py
+++ env_config/pool.py
@@ -20,7 +20,7 @@ COMMIT_POOL_ID = 'covid-r-commit-pool'
 COMMIT_POOL_VM_SIZE = 'STANDARD_D4_V3'
 
 COMMIT_POOL_START_TASK = f'''/bin/bash -c "cd ${{AZ_BATCH_NODE_SHARED_DIR}} &&
-git clone -b batch_test --depth 100 https://{GITHUB_USER_NAME}:{GITHUB_TOKEN}@github.com/epiforecasts/covid-rt-estimates.git &&
+git clone --depth 100 https://{GITHUB_USER_NAME}:{GITHUB_TOKEN}@github.com/epiforecasts/covid-rt-estimates.git &&
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 "'''
 COMMIT_POOL_SCALE_FORMULA = f"""startingNumberOfVMs = 0;
