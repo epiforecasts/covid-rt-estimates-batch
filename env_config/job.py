714a269a3a20c877da3a8d6af380c3b67a4cfb7b
Joe Hickson <joeHickson@users.noreply.github.com>
2021-07-19 09:33:52 +0100

    hotfix: update to new batch optimised version

--- env_config/job.py
+++ env_config/job.py
@@ -1,4 +1,4 @@
-PROCESSING_JOB_ID = 'covid-r-job'  # This needs a better name
+PROCESSING_JOB_ID = 'covid-r-job-ubuntu-20-04'  # This needs a better name
 COMMIT_JOB_ID = 'covid-r-commit'
 CONFIG_FILE = "_config.json"
 CONFIG_FOLDER = "json-configs"
